4e9fe493ff1b8427c534cbbe016bab387e4181bd
Isabella <isabellazapatapulgarin@gmail.com>
2022-11-20 15:28:08 -0500

    Add Pay and Facture components

--- src/Containers/Page/Pay/Pay.jsx
+++ src/Containers/Page/Pay/Pay.jsx
@@ -1,12 +1,45 @@
 import React from 'react'
-import Header from '../../../Components/Header'
+import './style.css'
+import './Pay.css'
+import { useNavigate } from 'react-router-dom'
 
 const Pay = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/")
+  }
+
   return (
-    
-    <div>
-        <Header/>
-    </div>
+    <>
+      <div className="container-form-tarjet" onSubmit={handleSubmit}>
+        <form action="" id="formulario-tarjeta" className="formulario-tarjeta">
+          <h1>Metodo de Pago</h1>
+          <div className="grupo">
+            <label htmlFor="inputNumero">Número Tarjeta</label>
+            <input type="text" id="inputNumero" maxLength="19" autoComplete="off" pattern='^(?:(\d{4}\s?){4}|(\d{4,6}\s?){3})$' required/>
+          </div>
+          <div className="grupo">
+            <label htmlFor="inputNombre">Nombre</label>
+            <input type="text" id="inputNombre" maxLength="19" autoComplete="off" required/>
+          </div>
+          <div className="flexbox">
+            <div className="grupo expira">
+              <label htmlFor="expired-day">Fecha de Expiracion</label>
+              <div className="flexbox">
+                <input type="text" id='expired-day' required/>
+              </div>
+            </div>
+
+            <div className="grupo ccv">
+              <label htmlFor="inputCCV">CCV</label>
+              <input type="text" id="inputCCV" maxLength="3" required/>
+            </div>
+          </div>
+          <button type="submit" className="btn-enviar">Enviar</button>
+        </form>
+      </div>
+  </>
   )
 }
 
--- src/Containers/app/App.js
+++ src/Containers/app/App.js
@@ -1,32 +1,22 @@
-<<<<<<< HEAD
 import Facture from '../Page/Facture/Facture';
-=======
-// import Buy from '../Page/Buy';
->>>>>>> 6bcd99deeb79952b17c7cae3a28f6ed700f8566a
 import './App.css';
 import Home from '../Page/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 import User from '../Page/User/User';
+import Pay from '../Page/Pay/Pay';
 
 // import Header from '../../Components/Header';
 
 function App() {
   return (
     <div>
-<<<<<<< HEAD
-
-      {/* <Home/> */}
-      {/* <Header/> */}
-        <Facture/>
-=======
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/User" element={<User/>}/>
-        {/* <Header/>
-        <Buy/> */}
+        <Route path='/Facture' element={<Facture/>}/>
+        <Route path='/Pay' element={<Pay/>}/>
       </Routes>
       
->>>>>>> 6bcd99deeb79952b17c7cae3a28f6ed700f8566a
     </div>
   );
 }
